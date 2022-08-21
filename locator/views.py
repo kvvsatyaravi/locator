ed9abdd43f8206f3325248838cbf68fbedb5418a
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2022-08-21 22:45:42 +0530

    trying ip count

--- locator/views.py
+++ locator/views.py
@@ -32,13 +32,23 @@ def js_passing(request):
 
         with open('./data.json','r+') as file:
             # First we load existing data into a dict.
+
             file_data = json.load(file)
-            
             users_temp = file_data["users"]
+            visitor_ip_address = file_data["ipaddresses"]  
             ip_addresses_temp = []
+            
+            if ipaddress in visitor_ip_address:
+                print("already ip address added in data.json")
+            else:
+                visitor_ip_address.append(ipaddress)
+                json.dump(file_data, file)
+
+
             for i in range(len(users_temp)):
                 ip_addresses_temp.append(users_temp[i]['ip'])
             print(ip_addresses_temp)
+            
 
             if(len(ip_addresses_temp) == 0):
                 file_data["users"].append(results)
@@ -49,6 +59,7 @@ def js_passing(request):
             else:
                 if ipaddress in ip_addresses_temp:
                     print("you have already visited these site")
+                    results["visited"] = "you already visited these website"
                 else:
                     file_data["users"].append(results)
                     # Sets file's current position at offset.
