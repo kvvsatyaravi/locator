cce604197c571a59a1b2a6da28f6ab93c3e79800
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2022-08-28 09:29:25 +0530

    backend logics ok

--- locator/views.py
+++ locator/views.py
@@ -1,22 +1,39 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 from geopy.geocoders import Nominatim
-import json,socket
-import requests
+import json
+
+ipaddress = ''
+visitor_ip_address = []
 
 def index(request):
     Template_get="index.html"
     if request.method=='GET':
-        
         return render(request,Template_get)
     
     if request.method == 'POST':
-        userip = request.POST['user_ip']
+        ipaddress = request.POST['user_ip']
+        users_data = {"visitorip": ipaddress ,"visited": 0}
+        with open('./data.json','r+') as file:
+            # First we load existing data into a dict.
+
+            load_data = json.load(file)
+            visitor_ip_address = load_data["ipaddresses"]
+            users_data["no_visitors"] = len(visitor_ip_address)
+            
+            if ipaddress in visitor_ip_address:
+                print("you already visited these website")
+                users_data["visited"] = 1 
+            else:
+                load_data["ipaddresses"].append(ipaddress)
+                file.seek(0)
+                json.dump(load_data, file, indent=4)
+
+        return HttpResponse(json.dumps(users_data), content_type="application/json") 
 
 		
 def js_passing(request):
     if request.method == 'POST':
-        ipaddress = request.POST['ipaddress']
         latitude_get = request.POST['latitude']
         longitude_get = request.POST['longitude']
         geolocator = Nominatim(user_agent="geoapi")
@@ -39,31 +56,16 @@ def js_passing(request):
 
             file_data = json.load(file)
             users_temp = file_data["users"]
-            visitor_ip_address = file_data["ipaddresses"]  
-            ip_addresses_temp = []
-            
-            if ipaddress in visitor_ip_address:
-                print("already ip address added in data.json")
-            else:
-                visitor_ip_address.append(ipaddress)
-                json.dump(file_data, file)
 
-
-            for i in range(len(users_temp)):
-                ip_addresses_temp.append(users_temp[i]['ip'])
-            print(ip_addresses_temp)
-            
-
-            if(len(ip_addresses_temp) == 0):
+            if(len(visitor_ip_address) == 0):
                 file_data["users"].append(results)
                 # Sets file's current position at offset.
                 file.seek(0)
                 # convert back to json.
                 json.dump(file_data, file, indent = 4)
             else:
-                if ipaddress in ip_addresses_temp:
+                if ipaddress in visitor_ip_address:
                     print("you have already visited these site")
-                    results["visited"] = "you already visited these website"
                 else:
                     file_data["users"].append(results)
                     # Sets file's current position at offset.
