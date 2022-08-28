8bba32907641bf08e67f6b9a200e93f658b4d1eb
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2022-08-28 17:03:23 +0530

    95% completed

--- locator/urls.py
+++ locator/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -23,4 +24,4 @@ urlpatterns = [
     path('getip/', views.index, name="getip"),
     path('post/', views.js_passing, name="jspassingvariable"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
